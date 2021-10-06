a6bd0fcdf2df8133c3d19f095e6b70bf4b11ff1c
Nguyen To <khoinguyent@gmail.com>
2021-10-06 17:10:15 +0700

    .

--- I3D/test_i3d.py
+++ I3D/test_i3d.py
@@ -55,7 +55,7 @@ def run(root, train_split, rgb_weights, flow_weights, configs):
     rgb_weights = torch.load(flow_weights)
     
     i3d_rgb.load_state_dict(rgb_weights, strict=True)
-    i3d_flow.load_state_dict(rgb_weights, strict=True)
+    i3d_flow.load_state_dict(flow_weights, strict=True)
 
     #cuda
     i3d_rgb.cuda()
