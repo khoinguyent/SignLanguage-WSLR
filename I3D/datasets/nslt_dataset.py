71794420aaa8714e338195e2240ca08245a711cb
Nguyen To <khoinguyent@gmail.com>
2021-10-06 16:21:54 +0700

    test script

--- I3D/datasets/nslt_dataset.py
+++ I3D/datasets/nslt_dataset.py
@@ -10,7 +10,7 @@ import torch
 import torch.utils.data as data_utl
 from videoprocessing import VideoProcessing as vp
 
-def make_dataset(split_file, split, root, mode, num_classes, rate = 1):
+def make_dataset(split_file, split, root, num_classes, rate = 1):
     dataset = []
     with open(split_file, 'r') as f:
         data = json.load(f)
@@ -166,3 +166,96 @@ class NSLT(data_utl.Dataset):
 
         return padded_imgs, label
 
+class NSLT_Test(data_utl.Dataset):
+
+    def __init__(self, split_file, split, root, transforms=None, rate=1):
+        self.num_classes = get_num_class(split_file)
+
+        self.rate = rate
+        self.data = make_dataset(split_file, split, root, num_classes=self.num_classes, rate = self.rate)
+        self.split_file = split_file
+        self.transforms = transforms
+        self.root = root
+
+    def __getitem__(self, index):
+        """
+        Args:
+            index (int): Index
+
+        Returns:
+            tuple: (image, target) where target is class_index of the target class.
+        """
+        vid, label, src, start_frame, nf = self.data[index]
+
+        total_frames = 45
+
+        try:
+            start_f = random.randint(0, nf - total_frames - 1) + start_frame
+        except ValueError:
+            start_f = start_frame
+            
+        rgb_imgs = vp.load_rgb_frames_from_video(self.root['word'], vid, start_f, total_frames)
+        flow_imgs = vp.load_flow_frames_upd(self.root['word'], vid, start_f, total_frames, self.rate)
+
+        #print(vid, imgs.shape)
+        rgb_imgs, label = self.pad(rgb_imgs, label, total_frames)
+        flow_imgs, label = self.pad(flow_imgs, label, total_frames)
+
+        rgb_imgs = self.transforms(rgb_imgs)
+        flow_imgs = self.transforms(flow_imgs)
+
+        ret_lab = torch.from_numpy(label)
+        ret_rgb_img = vp.video_to_tensor(rgb_imgs)
+        ret_flow_img = vp.video_to_tensor(flow_imgs)
+
+        return ret_rgb_img, ret_flow_img, ret_lab, vid
+
+    def __len__(self):
+        return len(self.data)
+
+    def pad(self, imgs, label, total_frames):
+        if imgs.shape[0] < total_frames:
+            num_padding = total_frames - imgs.shape[0]
+
+            if num_padding:
+                prob = np.random.random_sample()
+                if prob > 0.5:
+                    pad_img = imgs[0]
+                    pad = np.tile(np.expand_dims(pad_img, axis=0), (num_padding, 1, 1, 1))
+                    padded_imgs = np.concatenate([imgs, pad], axis=0)
+                else:
+                    pad_img = imgs[-1]
+                    pad = np.tile(np.expand_dims(pad_img, axis=0), (num_padding, 1, 1, 1))
+                    padded_imgs = np.concatenate([imgs, pad], axis=0)
+        else:
+            padded_imgs = imgs
+
+        label = label[:, 0]
+        label = np.tile(label, (total_frames, 1)).transpose((1, 0))
+
+        return padded_imgs, label
+
+    @staticmethod
+    def pad_wrap(imgs, label, total_frames):
+        if imgs.shape[0] < total_frames:
+            num_padding = total_frames - imgs.shape[0]
+
+            if num_padding:
+                pad = imgs[:min(num_padding, imgs.shape[0])]
+                k = num_padding // imgs.shape[0]
+                tail = num_padding % imgs.shape[0]
+
+                pad2 = imgs[:tail]
+                if k > 0:
+                    pad1 = np.array(k * [pad])[0]
+
+                    padded_imgs = np.concatenate([imgs, pad1, pad2], axis=0)
+                else:
+                    padded_imgs = np.concatenate([imgs, pad2], axis=0)
+        else:
+            padded_imgs = imgs
+
+        label = label[:, 0]
+        label = np.tile(label, (total_frames, 1)).transpose((1, 0))
+
+        return padded_imgs, label
--- I3D/test_i3d.py
+++ I3D/test_i3d.py
@@ -10,19 +10,17 @@ from torch.optim.lr_scheduler import StepLR, MultiStepLR
 
 from torchvision import transforms
 import videotransforms
-import videoprocessing
 import pandas as pd
-
 import numpy as np
 
 from config import Config
 from pytorch_i3d import InceptionI3d
-from pytorch_mlp import MLP
 
+# from datasets.nslt_dataset import NSLT as Dataset
+from datasets.nslt_dataset import NSLT_Test as Dataset
 
 os.environ["CUDA_DEVICE_ORDER"] = "PCI_BUS_ID"
 os.environ["CUDA_VISIBLE_DEVICES"] = '0'
-
 
 torch.manual_seed(0)
 np.random.seed(0)
@@ -30,29 +28,31 @@ np.random.seed(0)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 
-def run(video_path, num_classes, weights):
-    video = video_path.split("/")[-1]
-    video_path = video_path[0:len(video_path) - (len(video) + 2)]
+def run(root, train_split, rgb_weights, flow_weights, configs):
+    correct = 0
+    correct_5 = 0
+    correct_10 = 0
 
-    inputs_rgb = videoprocessing.video_to_tensor(
-        videoprocessing.load_rgb_frames_from_video(video_path, video, 0, 64)
-    )
+    test_transforms = transforms.Compose([videotransforms.CenterCrop(224)])
 
-    inputs_flow = videoprocessing.video_to_tensor(
-        videoprocessing.load_flow_frames_upd(video_path, video, 0, 64)
-    )
+    dataset = Dataset(train_split, 'test', root, test_transforms)
+    dataloader = torch.utils.data.DataLoader(dataset, batch_size=configs.batch_size, shuffle=True, num_workers=0,
+                                             pin_memory=True)
 
+    num_classes = dataset.num_classes
     #load models
     i3d_flow = InceptionI3d(400, in_channels=2)
     i3d_rgb = InceptionI3d(400, in_channels=3)
-    mlp = MLP(num_classes, 64)
+
+    i3d_flow.replace_logits(num_classes)
+    i3d_rgb.replace_logits(num_classes)
 
     #load weights
-    weights = torch.load(weights)
-    print(weights)
-    i3d_rgb.load_state_dict(weights['rgb'], strict=False)
-    i3d_flow.load_state_dict(weights['flow'], strict=False)
-    mlp.load_state_dict(weights['mlp'], strict=False)
+    rgb_weights = torch.load(rgb_weights)
+    rgb_weights = torch.load(flow_weights)
+    
+    i3d_rgb.load_state_dict(rgb_weights, strict=True)
+    i3d_flow.load_state_dict(rgb_weights, strict=True)
 
     #cuda
     i3d_rgb.cuda()
@@ -61,44 +61,97 @@ def run(video_path, num_classes, weights):
     i3d_flow.cuda()
     i3d_flow = nn.DataParallel(i3d_flow)
 
-    mlp.cuda()
-    mlp = nn.DataParallel(mlp)
-
-    #set model to test state
     i3d_rgb.train(False)
     i3d_flow.train(False)
-    mlp.train(False)
-
-    inputs_rgb.cuda()
-    inputs_flow.cuda()
-
-    i3d_rgb.eval()
-    i3d_flow.eval()
-    mlp.eval()
-
-    t_rgb = inputs_rgb.size(2)
-    t_flow = inputs_flow.szie(2)
-
-    per_frame_logits_rgb = i3d_rgb(inputs_rgb)
-    per_frame_logits_rgb = F.upsample(per_frame_logits_rgb, t_rgb, mode='linear')
     
-    per_frame_logits_flow = i3d_flow(inputs_flow)
-    per_frame_logits_flow = F.upsample(per_frame_logits_flow, t_flow, mode='linear')
+    top1_fp = np.zeros(num_classes, dtype=np.int)
+    top1_tp = np.zeros(num_classes, dtype=np.int)
 
-    outputs = None
-    #put output of rgb stream and flow stream through MLP network
-    for i in range(0, per_frame_logits_flow.shape[2]):
-        input_mlp = torch.cat((per_frame_logits_rgb[:,:,i], per_frame_logits_flow[:,:,i]), 1)
-        output = mlp(input_mlp)
+    top5_fp = np.zeros(num_classes, dtype=np.int)
+    top5_tp = np.zeros(num_classes, dtype=np.int)
 
-        if i == 0:
-            outputs = output
+    top10_fp = np.zeros(num_classes, dtype=np.int)
+    top10_tp = np.zeros(num_classes, dtype=np.int)
+
+    for data in dataloader:
+        if data == -1: # bracewell does not compile opencv with ffmpeg, strange errors occur resulting in no video loaded
+            continue
+
+        # inputs, labels, vid, src = data
+        rgb_inputs, flow_inputs, labels, vid = data
+
+        # wrap them in Variable
+        rgb_inputs = rgb_inputs.cuda()
+        t1 = rgb_inputs.size(2)
+
+        flow_inputs = flow_inputs.cuda()
+        t2 = flow_inputs.size(2)
+
+        labels = labels.cuda()
+
+        per_rgb_frame_logits = i3d_rgb(rgb_inputs, pretrained=False)
+        # upsample to input size
+        per_rgb_frame_logits = F.upsample(per_rgb_frame_logits, t1, mode='linear')
+
+        per_flow_frame_logits = i3d_flow(flow_inputs, pretrained=False)
+        # upsample to input size
+        per_flow_frame_logits = F.upsample(per_flow_frame_logits, t2, mode='linear')
+
+        per_frame_logits = (per_rgb_frame_logits + per_flow_frame_logits) / 2.0
+        predictions = torch.mean(per_frame_logits, dim=2)[0]
+        out_labels = np.argsort(predictions.cpu().detach().numpy())
+
+        if labels[0].item() in out_labels[-5:]:
+            correct_5 += 1
+            top5_tp[labels[0].item()] += 1
         else:
-            outputs = torch.cat((outputs, output))
+            top5_fp[labels[0].item()] += 1
+        if labels[0].item() in out_labels[-10:]:
+            correct_10 += 1
+            top10_tp[labels[0].item()] += 1
+        else:
+            top10_fp[labels[0].item()] += 1
+        if torch.argmax(predictions).item() == labels[0].item():
+            correct += 1
+            top1_tp[labels[0].item()] += 1
+        else:
+            top1_fp[labels[0].item()] += 1
+        print(vid, float(correct) / len(dataloader), float(correct_5) / len(dataloader),
+              float(correct_10) / len(dataloader))
 
-    outputs = outputs.unsqueeze(0)
-    outputs = torch.transpose(outputs, 1, 2)
+    top1_per_class = np.mean(top1_tp / (top1_tp + top1_fp))
+    top5_per_class = np.mean(top5_tp / (top5_tp + top5_fp))
+    top10_per_class = np.mean(top10_tp / (top10_tp + top10_fp))
+    print('top-k average per class acc: {}, {}, {}'.format(top1_per_class, top5_per_class, top10_per_class))
 
-    predictions = torch.max(outputs, dim=2)[0]
 
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--rgb_weights', type=str, default=None)
+    parser.add_argument('--flow_weights', type=str, default=None)
+    parser.add_argument('--root', type=str, default={'word': '../data/WLASL2000'})
+    parser.add_argument('--config', type=str, default='configfiles/asl2000.ini')
+    parser.add_argument('--train_split', type=str, default='preprocess/nslt_2000.json')
 
+    args = parser.parse_args()
+
+    root = args.root
+    rgb_weights = args.rgb_weights
+    flow_weights = args.flow_weights
+    config_file = args.config
+    train_split = args.train_split
+
+    # WLASL setting
+    # mode = 'rgb'
+    # root = {'word': '../../data/WLASL2000'}
+
+    # save_model = 'checkpoints/'
+    # train_split = 'preprocess/nslt_2000.json'
+
+    # weights = 'archived/asl2000/FINAL_nslt_2000_iters=5104_top1=32.48_top5=57.31_top10=66.31.pt'
+    # weights = None
+    # config_file = 'configfiles/asl2000.ini'
+
+    configs = Config(config_file)
+    print(root, train_split)
+    run(configs=configs, root=root, train_split=train_split, rgb_weights=rgb_weights, flow_weights=flow_weights)
